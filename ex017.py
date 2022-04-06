c336a0400b2498df55ad7df6d8922de86d6191f8
ViniciusNC <viviwow2001@hotmail.com>
2022-04-06 16:57:18 -0300

    Studying Python

--- ex017.py
+++ ex017.py
@@ -1,9 +1,7 @@
-from math import sqrt
+import math
 n1 = float(input('Digite o Cateto Adjacente do Triângulo: '))
 n2 = float(input('Digite o Cateto Oposto do Triângulo: '))
-catetoadja = pow(n1, 2)
-catetoopos = pow(n2, 2)
-hipotenusa = sqrt((catetoadja + catetoopos))
-print(f'O comprimento da hipotenusa é: {hipotenusa:.2f}')
+hip = math.hypot(n2, n1)
+print(f'A hipotenusa vai medir: {hip:.2f}')
 
 
