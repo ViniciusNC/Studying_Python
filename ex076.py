a35650d73bb3296bb4d960473b3e7fbf2b3a71d6
Vinícius Neves Cunha <viviwow2001@hotmail.com>
2022-05-02 08:37:32 -0300

    estudos python

--- ex076.py
+++ ex076.py
@@ -1,1 +1,15 @@
-print('teste')
+expr = str(input('Digite uma expressão que utileza parenteses: '))
+lista = []
+for simbolos in expr:
+ if simbolos == '(':
+    lista.append('(')
+ elif simbolos == ')':
+  if len(lista) > 0:
+     lista.pop()
+  else:
+      lista.append(')')
+      break
+if len(lista) == 0:
+    print('Sua expressão esta valida')
+else:
+    print('Sua expressão esta inválida')
